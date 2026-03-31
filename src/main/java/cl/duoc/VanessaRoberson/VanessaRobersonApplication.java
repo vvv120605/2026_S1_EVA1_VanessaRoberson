f21e729d8a7f2c8e0853f8fa848428b83c83497c
Vanessa <va.roberson@duocuc.cl>
2026-03-31 10:37:20 -0300

    primer commit

---
+++ src/main/java/cl/duoc/VanessaRoberson/VanessaRobersonApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.VanessaRoberson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanessaRobersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanessaRobersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/VanessaRoberson/VanessaRobersonApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.VanessaRoberson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanessaRobersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
