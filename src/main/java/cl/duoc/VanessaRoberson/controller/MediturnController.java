eae887f02fa9ebabb243a2e28843c5cb6fd7bf30
Vanessa <va.roberson@duocuc.cl>
2026-03-31 12:03:06 -0300

    Controller a la mitad 22222

--- src/main/java/cl/duoc/VanessaRoberson/controller/MediturnController.java
+++ src/main/java/cl/duoc/VanessaRoberson/controller/MediturnController.java
@@ -1,5 +1,28 @@
 package cl.duoc.VanessaRoberson.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.duoc.VanessaRoberson.model.Mediturn;
+import cl.duoc.VanessaRoberson.service.MediturnService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping
 public class MediturnController {
+    @Autowired
+    private MediturnService mediturnService;
+
+    @GetMapping
+    public ResponseEntity<List<Mediturn>> getAllPatients(){
+        return ResponseEntity.ok(mediturnService.getAllPatients());
+    }
+    
 
 }
